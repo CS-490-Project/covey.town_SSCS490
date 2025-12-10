430d3cdafb3ecf6f290936c86d3377205d65f7c4
Abigail Goz <abigail@dogbuilt.net>
2025-12-10 14:51:08 -0500

    Make _emitAreaChanged unconditional in songEnd

--- townService/src/town/JukeboxArea.ts
+++ townService/src/town/JukeboxArea.ts
@@ -233,9 +233,9 @@ export default class JukeboxArea extends InteractableArea {
       this.songQueue[0].startedAt = Date.now();
 
       this._songEndTimeout = setTimeout(() => this._songEnd(), this.songQueue[0].duration);
-
-      this._emitAreaChanged();
     }
+
+    this._emitAreaChanged();
   }
 
   /**
