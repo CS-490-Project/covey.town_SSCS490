3e4f9e33119bdcbcdb0d6bc93823c3402ec1b504
Shreyal Sharma <157657346+shreyal-sharma@users.noreply.github.com>
2025-12-10 14:17:57 -0500

    Feature/searchbar addition (#36)
    * README file updated with archeticture diagram
    
    * SWAD and HJKL controls paused when interacting with search bar on shared playlist

--- frontend/src/components/Town/interactables/JukeboxAreaMusic.tsx
+++ frontend/src/components/Town/interactables/JukeboxAreaMusic.tsx
@@ -547,12 +547,12 @@ export default function JukeboxAreaWrapper(): JSX.Element {
   const [searchQuery, setSearchQuery] = useState('');
 
   useEffect(() => {
-    if (jukeboxArea) {
+    if (jukeboxArea && !isHidden) {
       townController.pause();
     } else {
       townController.unPause();
     }
-  }, [townController, jukeboxArea]);
+  }, [townController, jukeboxArea, isHidden]);
 
   const handleSearch = useCallback(
     async (query: string) => {
@@ -585,13 +585,15 @@ export default function JukeboxAreaWrapper(): JSX.Element {
     [toast, townController, jukeboxArea, setSearchQuery],
   );
 
-  function setHide() {
+  const setHide = useCallback(() => {
     setIsHidden(true);
-  }
+    townController.unPause();
+  }, [townController]);
 
-  function setShow() {
+  const setShow = useCallback(() => {
     setIsHidden(false);
-  }
+    townController.pause();
+  }, [townController]);
 
   useEffect(() => {
     console.log('Change');
@@ -602,7 +604,7 @@ export default function JukeboxAreaWrapper(): JSX.Element {
       jukeboxArea?.removeListener('hide', setHide);
       jukeboxArea?.removeListener('show', setShow);
     };
-  }, [jukeboxArea]);
+  }, [jukeboxArea, setHide, setShow]);
 
   // Playback controls
   const handlePlayPause = useCallback(() => {
